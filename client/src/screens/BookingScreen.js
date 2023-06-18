d82f1b4ddab06e189da366d9457a8c05bb0258a7
Amartya Sen <amartyasen895@gmail.com>
2023-06-19 02:04:31 +0530

    added Booking Screen details

--- client/src/screens/BookingScreen.js
+++ client/src/screens/BookingScreen.js
@@ -1,38 +1,90 @@
-import React ,{useState , useEffect} from 'react'
-import { useParams } from 'react-router-dom'
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import {Carousel} from 'react-bootstrap'
 
 function BookingScreen() {
   const { roomid } = useParams();
-  const [loading, setLoading] = useState();
+  const [loading, setLoading] = useState(true);
   const [error, setError] = useState();
-  const [room , setRoom] = useState();
+  const [room, setRoom] = useState();
 
-  useEffect(()=>{
+  useEffect(() => {
     const fetchData = async () => {
-        try {
-          setLoading(true);
-          const data = (await axios.post("/api/rooms/getroombyid",{roomid})).data;
-          setRoom(data);
+      try {
+        setLoading(true);
+        const data = (await axios.post("/api/rooms/getroombyid", { roomid }))
+          .data;
+        setRoom(data);
         //   console.log(data)
-          setLoading(false);
-        } catch (error) {
-          setError(error);
-          // console.log(error);
-          setLoading(false);
-        }
-      };
-  
-      fetchData();
-  },[])
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+        // console.log(error);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
-    <div>
-      <h1>Booking Screen</h1>
-      <h1>Room id: {roomid}</h1>
-      <h1>Name</h1>
+    <div className="m-5">
+      {loading ? (
+        <h1>Loading..</h1>
+      ) : error ? (
+        <h1>Error..</h1>
+      ) : (
+        <div>
+          <div className="row justify-content-center mt-5  box-shadow">
+          <div style={{}} className="col-md-6">
+            <h1 style={{textAlign:'center'}}>{room.name}</h1>
+            <Carousel>
+            {room.imagesUrl.map(url=>{
+              return(
+                <Carousel.Item>
+                <img
+                  className="d-block w-100  bigimg"
+                  src={url}
+                  alt="First slide"
+                />   
+              </Carousel.Item>
+              );
+            })}
+          </Carousel>
+          </div>
+          <div className="col-md-5">
+            <h1>Booking Details</h1>
+            <hr />
+            <b>
+            <p>Name : Amartya </p>
+            <p>From Date : 19-03-2023 </p>
+            <p>To Date : 21-03-2023</p>
+            <p>Max Count : {room.maxcount}</p>
+
+            </b>
+
+            <div>
+              <h1>Amount</h1>
+              <hr />
+              <b>
+              <p>Total Days : 3</p>
+              <p>Rent Per Day : {room.rentPerDay}</p>
+              <p>Total Amount : {room.rentPerDay * 3}</p>
+              </b>
+
+              <div>
+                <button style={{float:'right'}} className="btn btn-primary">Pay Now</button>
+              </div>
+            </div>
+
+          </div>
+          </div>
+         
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default BookingScreen
+export default BookingScreen;
