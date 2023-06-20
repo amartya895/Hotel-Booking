b46f71ecdb9461ddcb9a72c0024dd9370675f692
Amartya Sen <amartyasen895@gmail.com>
2023-06-21 00:39:21 +0530

    Login and Register Updated

--- client/src/screens/LoginScreen.js
+++ client/src/screens/LoginScreen.js
@@ -1,9 +1,49 @@
-import React from 'react'
+import React,{useState } from 'react'
 
 function LoginScreen() {
+  
+    const [email , setemail] = useState('');
+    const [password , setPassword] = useState('');
+    
+
+    const Login = ()=>{
+        
+            const userData ={
+               email, password
+            }
+    
+            console.log(`${userData} logged Successfully`);
+        
+    
+    }
   return (
     <div>
-      
+      <div className="row justify-content-center mt-5">
+        <div className="col-md-5">
+          <div className="box-shadow p-3 mt-5">
+            <h1 className="register-head">Login Here</h1>
+            
+            <input
+              type="email"
+            value={email}
+              className="form-control"
+              id=""
+              onChange={(e)=>setemail(e.target.value)}
+              placeholder="Email"
+            />
+            <input
+              type="password"
+              value={password}
+              onChange={(e)=>setPassword(e.target.value)}
+              className="form-control"
+              placeholder="Password"
+              id=""
+            />
+           
+            <button className="btn btn-primary mt-3" onClick={Login}>Login</button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- client/src/screens/RegisterScreen.js
+++ client/src/screens/RegisterScreen.js
@@ -1,26 +1,70 @@
-import React from 'react'
+import React, { useState } from "react";
 
 function RegisterScreen() {
+    const [name , setName] = useState('');
+    const [email , setemail] = useState('');
+    const [password , setPassword] = useState('');
+    const [cpassword , setCpassword] = useState('');
+
+    const Register = ()=>{
+        if(password === cpassword)
+        {
+            const userData ={
+                name,email, password, cpassword
+            }
+    
+            console.log(userData);
+        }
+        else{
+            console.log("password didnt matched");
+        }
+    
+    }
+
   return (
     <div>
-      <form>
-  <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-  </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
-  </div>
-  <div class="form-group form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+      <div className="row justify-content-center mt-5">
+        <div className="col-md-5">
+          <div className="box-shadow p-3 mt-5">
+            <h1 className="register-head">Register Here</h1>
+            <input
+              type="text"
+                value={name}
+              className="form-control"
+              onChange={(e)=>setName(e.target.value)}
+              id=""
+              placeholder="Name"
+            />
+            <input
+              type="email"
+            value={email}
+              className="form-control"
+              id=""
+              onChange={(e)=>setemail(e.target.value)}
+              placeholder="Email"
+            />
+            <input
+              type="password"
+              value={password}
+              onChange={(e)=>setPassword(e.target.value)}
+              className="form-control"
+              placeholder="Password"
+              id=""
+            />
+            <input
+              type="password"
+              value={cpassword}
+              onChange={(e)=>setCpassword(e.target.value)}
+              className="form-control"
+              placeholder="Re-Enter Password"
+              id=""
+            />
+            <button className="btn btn-primary mt-3" onClick={Register}>Register</button>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default RegisterScreen
+export default RegisterScreen;
