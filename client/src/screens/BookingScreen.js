563c29bc3e51f8026f25b05e876ef21ebb747efa
Amartya Sen <amartyasen895@gmail.com>
2023-06-30 00:07:07 +0530

    stripe payment gateway

--- client/src/screens/BookingScreen.js
+++ client/src/screens/BookingScreen.js
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import axios from 'axios';
-import { Carousel } from 'react-bootstrap';
-import Loader from '../components/Loader';
-import moment from 'moment';
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { Carousel } from "react-bootstrap";
+import Loader from "../components/Loader";
+import moment from "moment";
+import StripeCheckout from "react-stripe-checkout";
 
 function BookingScreen() {
   const { roomid, fromDate, toDate } = useParams();
@@ -11,15 +12,15 @@ function BookingScreen() {
   const [error, setError] = useState();
   const [room, setRoom] = useState();
   const [totalAmount, setTotalAmount] = useState();
-  const fromDateObj = moment(fromDate, 'DD-MM-YYYY');
-  const toDateObj = moment(toDate, 'DD-MM-YYYY');
-  const totalDays = toDateObj.diff(fromDateObj, 'days');
+  const fromDateObj = moment(fromDate, "DD-MM-YYYY");
+  const toDateObj = moment(toDate, "DD-MM-YYYY");
+  const totalDays = toDateObj.diff(fromDateObj, "days");
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         setLoading(true);
-        const { data } = await axios.post('/api/rooms/getroombyid', { roomid });
+        const { data } = await axios.post("/api/rooms/getroombyid", { roomid });
         setRoom(data);
         setLoading(false);
         setTotalAmount(data.rentPerDay * (totalDays + 1));
@@ -30,21 +31,23 @@ function BookingScreen() {
     };
 
     fetchData();
-  }, [roomid,totalDays]);
+  }, [roomid, totalDays]);
 
-  const bookRoom = async () => {
+  const onToken = async (token) => {
+    console.log(token);
     const bookingDetails = {
       room: room,
-      userid: JSON.parse(localStorage.getItem('currentUser'))._id,
+      userid: JSON.parse(localStorage.getItem("currentUser"))._id,
       fromDate: fromDate,
       toDate: toDate,
       totalAmount: totalAmount,
       totalDays: totalDays + 1,
+      token,
     };
 
     try {
       console.log(bookingDetails);
-      const result = await axios.post('/api/bookings/bookroom', bookingDetails);
+      const result = await axios.post("/api/bookings/bookroom", bookingDetails);
       console.log(result);
     } catch (error) {
       console.error(error);
@@ -61,11 +64,15 @@ function BookingScreen() {
         <div>
           <div className="row justify-content-center mt-5 box-shadow">
             <div className="col-md-6">
-              <h1 style={{ textAlign: 'center' }}>{room.name}</h1>
+              <h1 style={{ textAlign: "center" }}>{room.name}</h1>
               <Carousel>
                 {room.imagesUrl.map((url) => (
                   <Carousel.Item key={url}>
-                    <img className="d-block w-100 bigimg" src={url} alt="Slide" />
+                    <img
+                      className="d-block w-100 bigimg"
+                      src={url}
+                      alt="Slide"
+                    />
                   </Carousel.Item>
                 ))}
               </Carousel>
@@ -74,7 +81,9 @@ function BookingScreen() {
               <h1>Booking Details</h1>
               <hr />
               <b>
-                <p>Name: {JSON.parse(localStorage.getItem('currentUser')).name}</p>
+                <p>
+                  Name: {JSON.parse(localStorage.getItem("currentUser")).name}
+                </p>
                 <p>From Date: {fromDate}</p>
                 <p>To Date: {toDate}</p>
                 <p>Max Count: {room.maxcount}</p>
@@ -90,9 +99,19 @@ function BookingScreen() {
                 </b>
 
                 <div>
-                  <button style={{ float: 'right' }} className="btn btn-primary" onClick={bookRoom}>
-                    Pay Now
-                  </button>
+                  <StripeCheckout
+                    amount={totalAmount * 100}
+                    currency="INR"
+                    token={onToken}
+                    stripeKey="pk_test_51NOHfcSCaNtjKQutgNPvWOQ8Ipd942UWkuZAi6ce5hln5Mh5ukgdQYX7YAXMCChapeDn4DUXkRBiKV3QUBC0HrPS00vhRIdq2b"
+                  >
+                    <button
+                      style={{ float: "right" }}
+                      className="btn btn-primary"
+                    >
+                      Pay Now
+                    </button>
+                  </StripeCheckout>
                 </div>
               </div>
             </div>
--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -3,19 +3,20 @@ import axios from "axios";
 import Room from "../components/Room";
 import Loader from "../components/Loader";
 import Error from "../components/Error";
-import { DatePicker, Space } from 'antd';
-import moment from 'moment';
+import { DatePicker } from "antd";
+import moment from "moment";
 
 const { RangePicker } = DatePicker;
 
+// pk_test_51NOHfcSCaNtjKQutgNPvWOQ8Ipd942UWkuZAi6ce5hln5Mh5ukgdQYX7YAXMCChapeDn4DUXkRBiKV3QUBC0HrPS00vhRIdq2b
 
 function HomeScreen() {
   const [rooms, setRooms] = useState([]);
   const [loading, setLoading] = useState();
   const [error, setError] = useState();
-  const [fromDate , setFromDate] = useState();
-  const [toDate , setToDate] = useState();
-  const [dupilcateRooms , setDuplicateRooms] = useState();
+  const [fromDate, setFromDate] = useState();
+  const [toDate, setToDate] = useState();
+  const [dupilcateRooms, setDuplicateRooms] = useState();
 
   useEffect(() => {
     const fetchData = async () => {
@@ -23,7 +24,7 @@ function HomeScreen() {
         setLoading(true);
         const data = (await axios.get("/api/rooms/getAllRooms")).data;
         setRooms(data);
-        setDuplicateRooms(data)
+        setDuplicateRooms(data);
         setLoading(false);
       } catch (error) {
         setError(error);
@@ -35,69 +36,68 @@ function HomeScreen() {
   }, []);
 
   const filterByDate = (dates) => {
-     //from date
-     console.log(dates[0].format("DD-MM-YYYY"));
-     setFromDate(dates[0].format("DD-MM-YYYY"));
-     //to date
-     console.log(dates[1].format("DD-MM-YYYY"));
-     setToDate(dates[1].format("DD-MM-YYYY"));
-   
-     //tempRooms
-     var tempRooms = [];
-   
-     for (const room of dupilcateRooms) {
-       var availability = false;
-   
-       if (room.currentBookings.length > 0) {
-         for ( const booking of room.currentBookings) {
-           //check between or equal to dates
-           if (
-             !moment(moment(dates[0]).format("DD-MM-YYYY")).isBetween(
-               booking.fromDate,
-               booking.toDate
-             ) &&
-             !moment(moment(dates[1]).format("DD-MM-YYYY")).isBetween(
-               booking.fromDate,
-               booking.toDate
-             )
-           ) {
-             
-             if (
-               dates[0].format("DD-MM-YYYY") !== booking.fromDate &&
-               dates[0].format("DD-MM-YYYY") !== booking.toDate &&
-               dates[1].format("DD-MM-YYYY") !== booking.fromDate &&
-               dates[1].format("DD-MM-YYYY") !== booking.toDate
-             ) {
-               availability = true;
-             }
-           }
-         }
-       } else {
-         availability = true;
-       }
-   
-       if (availability === true) {
-         tempRooms.push(room);
-       }
-     }
-   
-     setRooms(tempRooms);
+    //from date
+    console.log(dates[0].format("DD-MM-YYYY"));
+    setFromDate(dates[0].format("DD-MM-YYYY"));
+    //to date
+    console.log(dates[1].format("DD-MM-YYYY"));
+    setToDate(dates[1].format("DD-MM-YYYY"));
+
+    //tempRooms
+    var tempRooms = [];
+
+    for (const room of dupilcateRooms) {
+      var availability = false;
+
+      if (room.currentBookings.length > 0) {
+        for (const booking of room.currentBookings) {
+          //check between or equal to dates
+          if (
+            !moment(moment(dates[0]).format("DD-MM-YYYY")).isBetween(
+              booking.fromDate,
+              booking.toDate
+            ) &&
+            !moment(moment(dates[1]).format("DD-MM-YYYY")).isBetween(
+              booking.fromDate,
+              booking.toDate
+            )
+          ) {
+            if (
+              dates[0].format("DD-MM-YYYY") !== booking.fromDate &&
+              dates[0].format("DD-MM-YYYY") !== booking.toDate &&
+              dates[1].format("DD-MM-YYYY") !== booking.fromDate &&
+              dates[1].format("DD-MM-YYYY") !== booking.toDate
+            ) {
+              availability = true;
+            }
+          }
+        }
+      } else {
+        availability = true;
+      }
+
+      if (availability === true) {
+        tempRooms.push(room);
+      }
+    }
+
+    setRooms(tempRooms);
   };
-  
-  
 
   return (
     <div className="container">
       <div className="row">
         <div className="col-md-3 mt-3">
-          <RangePicker format='DD-MM-YYYY' onChange={filterByDate}/>
+          <RangePicker format="DD-MM-YYYY" onChange={filterByDate} />
         </div>
       </div>
       <div className="row justify-content-center mt-5">
         {loading ? (
-         <Loader/>
+          <Loader />
         ) : error ? (
-          <h1><Error/></h1>
+          <h1>
+            <Error />
+          </h1>
         ) : (
           rooms.map((room) => {
             return (
