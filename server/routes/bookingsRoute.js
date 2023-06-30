8bc79864a4a365927667ca196f43feac9c578526
Amartya Sen <amartyasen895@gmail.com>
2023-06-30 23:37:39 +0530

    updated payment gateway

--- server/routes/bookingsRoute.js
+++ server/routes/bookingsRoute.js
@@ -32,32 +32,27 @@ router.post("/bookroom", async (req, resp) => {
     );
 
     if (payment) {
-      try {
-        const newBooking = new Booking({
-          room: room.name,
-          roomid: room._id,
-          userid: userid,
-          fromdate: fromDate,
-          todate: toDate,
-          totalamount: totalAmount,
-          totaldays: totalDays,
-          transactionid: "1234",
-        });
+      const newBooking = new Booking({
+        room: room.name,
+        roomid: room._id,
+        userid: userid,
+        fromdate: fromDate,
+        todate: toDate,
+        totalamount: totalAmount,
+        totaldays: totalDays,
+        transactionid: "1234",
+      });
 
-        const booking = await newBooking.save();
-        const roomtemp = await Room.findOne({ _id: room._id });
-        roomtemp.currentBookings.push({
-          bookingid: booking._id,
-          fromDate,
-          toDate,
-          userid: userid,
-          status: booking.status,
-        });
-        await roomtemp.save();
-        resp.send("Room Booked Successfully");
-      } catch (error) {
-        return resp.status(400).json({ error: error.message });
-      }
+      const booking = await newBooking.save();
+      const roomtemp = await Room.findOne({ _id: room._id });
+      roomtemp.currentBookings.push({
+        bookingid: booking._id,
+        fromDate,
+        toDate,
+        userid: userid,
+        status: booking.status,
+      });
+      await roomtemp.save();
     }
 
     resp.send("Your Payment successfull, Booked Successfully");
