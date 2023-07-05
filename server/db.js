333dbbfc3d1290decd2ce39c7088b28c59da91c0
Amartya Sen <amartyasen895@gmail.com>
2023-07-05 22:20:48 +0530

    changed db

--- server/db.js
+++ server/db.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
-const mongoUrl = 'mongodb+srv://amartyasen:amartya@cluster0.tlqtekf.mongodb.net/mern-hotel';
+const mongoUrl = 'mongodb+srv://amartya:amartya@mern-hotel.ci0bbjk.mongodb.net/hotel-booking-system'
+// const mongoUrl = 'mongodb+srv://amartyasen:amartya@cluster0.tlqtekf.mongodb.net/mern-hotel';
+
+
 
 mongoose.connect(mongoUrl , {useUnifiedTopology:true, useNewUrlParser:true});
 
